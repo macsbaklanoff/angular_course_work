aa1b64baf5908f285cc9155026d339814697be15
macsbaklanoff <baklanov689@gmail.com>
2025-03-31 23:29:14 +0300

    update projects and data source for projects moved rxResource from component into data Source

--- src/app/data-sources/project.data-source.ts
+++ src/app/data-sources/project.data-source.ts
@@ -1,43 +1,93 @@
-import {inject} from '@angular/core';
+import {computed, inject, signal} from '@angular/core';
 import {ProjectService} from '../services/project.service';
 import {IPageRequest} from '../interfaces/page-request.interface';
 import {ISortRequest} from '../interfaces/sort-request.interface';
 import {IIssueFilterRequest} from '../interfaces/requests/issue/issue-filter-request.interface';
 import {IProjectCreateRequest} from '../interfaces/requests/project/project-create-request.interface';
 import {IProjectUpdateRequest} from '../interfaces/requests/project/update-project-request.interface';
+import {IProjectFilterRequest} from '../interfaces/requests/project/project-filter-request.interface';
+import {rxResource} from '@angular/core/rxjs-interop';
+import {formatDistanceToNow} from 'date-fns';
+import {PageEvent} from '@angular/material/paginator';
 
 
 export class ProjectDataSource {
 
   private readonly _projectService = inject(ProjectService);
 
+  public sortRequest = signal<ISortRequest>({
+    sortBy: 'code',
+    sortDir: 'asc'
+  });
+
+  public filterRequest = signal<IProjectFilterRequest>({});
+
+  public readonly pageRequest = signal<IPageRequest>({
+    pageNumber: 1,
+    pageSize: 5,
+  });
+
+  private readonly _projectResource = rxResource({
+    request: () => ({
+      pageRequest: this.pageRequest(),
+      sortRequest: this.sortRequest(),
+      filterRequest: this.filterRequest()
+    }),
+    loader: ({request}) =>
+      this._projectService.getProjects(request.pageRequest, request.sortRequest, request.filterRequest)
+  });
+
+  public readonly data = computed(() => {
+    return this._projectResource.value()?.items.map(project => ({
+      ...project,
+      createdOn: formatDistanceToNow(new Date(project.createdOn), {addSuffix: true}),
+      modifiedOn: formatDistanceToNow(new Date(project.modifiedOn), {addSuffix: true}),
+    })) ?? [];
+  });
+
+  public readonly total = computed(() => {
+    return this._projectResource.value()?.total ?? 0;
+  });
+
+  public readonly isLoading = computed(() => {
+    return this._projectResource.isLoading();
+  })
+
+  public onPageChange($event: PageEvent) {
+    this.pageRequest.set({
+      pageNumber: $event.pageIndex + 1,
+      pageSize: $event.pageSize,
+    })
+  }
+
+  public changeSort(sortBy: string) {
+    if (this.sortRequest().sortDir === 'asc') {
+      this.sortRequest.set({
+        sortBy: sortBy,
+        sortDir: "desc"
+      });
+    } else {
+      this.sortRequest.set({
+        sortBy: sortBy,
+        sortDir: "asc"
+      });
+    }
+  }
+
   public getProjects(pageRequest?: IPageRequest, sortRequest?: ISortRequest, filterRequest?: IIssueFilterRequest) {
     return this._projectService.getProjects(pageRequest, sortRequest, filterRequest);
   }
+
   public createProject(projectRequest: IProjectCreateRequest) {
     return this._projectService.createProject(projectRequest);
   }
+
   public updateProject(projectId: string, projectRequest: IProjectUpdateRequest) {
     return this._projectService.updateProject(projectId, projectRequest);
   }
+
   public deleteProject(projectId: string) {
     return this._projectService.deleteProject(projectId);
   }
-  // private readonly _projectResource = rxResource({
-  //   request: () => ({
-  //     pageRequest: this._pageRequest(),
-  //     sortRequest: this._sortRequest(),
-  //     filterRequest: this._filterRequest()
-  //   }),
-  //   loader: ({request}) =>
-  //     this._projectService.getProjects(request.pageRequest, request.sortRequest, request.filterRequest)
-  // });
-  //
-  // public readonly data = computed<IProjectResponse[]>(() => {
-  //   return this._projectResource.value()?.items ?? [];
-  // });
-  //
-  // public readonly isLoading = computed<boolean>(() => {
-  //   return this._projectResource.isLoading();
-  // })
+
 }
--- src/app/layout/project/project.component.ts
+++ src/app/layout/project/project.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, effect, inject, Signal, signal} from '@angular/core';
+import {Component, effect, inject, signal} from '@angular/core';
 import {ProjectDataSource} from '../../data-sources/project.data-source';
 import {MatTableModule} from '@angular/material/table';
 import {FormsModule} from '@angular/forms';
@@ -17,15 +17,10 @@ import {
   DeleteProjectDialogComponent
 } from '../dialogs/project-dialogs/delete-project-dialog/delete-project-dialog.component';
 import {IProjectCreateRequest} from '../../interfaces/requests/project/project-create-request.interface';
-import {IPageRequest} from '../../interfaces/page-request.interface';
-import {ISortRequest} from '../../interfaces/sort-request.interface';
-import {IProjectFilterRequest} from '../../interfaces/requests/project/project-filter-request.interface';
-import {IProjectResponse} from '../../interfaces/responses/project/project-response.interface';
 import {IProjectUpdateRequest} from '../../interfaces/requests/project/update-project-request.interface';
 import {MatPaginator, PageEvent} from '@angular/material/paginator';
-import {formatDistanceToNow} from 'date-fns';
 import {debounceTime} from 'rxjs/operators';
-import {rxResource, toObservable} from '@angular/core/rxjs-interop';
+import {toObservable} from '@angular/core/rxjs-interop';
 import {MatFormField, MatPrefix, MatSuffix} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 
@@ -44,7 +39,7 @@ import {MatInput} from '@angular/material/input';
     MatFormField,
     MatInput,
     MatPrefix,
-    MatSuffix
+    MatSuffix,
   ],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
@@ -52,24 +47,7 @@ import {MatInput} from '@angular/material/input';
 export class ProjectComponent {
   private readonly _matDialogRef = inject(MatDialog);
 
-  private readonly _pageRequest = signal<IPageRequest>({
-    pageNumber: 1,
-    pageSize: 5,
-  });
-
-  private readonly _sortRequest = signal<ISortRequest>({
-    sortBy: 'code',
-    sortDir: 'asc',
-  });
-
-  private readonly _filterRequest = signal<IProjectFilterRequest>({});
-
-
   public dataSource = new ProjectDataSource();
-
-  public sortRequest = computed(() => {
-    return this._sortRequest().sortDir;
-  })
 
   public searchTerm = signal<string>('');
 
@@ -80,10 +58,9 @@ export class ProjectComponent {
   displayedColumns: string[] = ['code', 'name', 'created', 'modified', 'actions'];
 
   constructor(private router: Router) {
-    this.load();
     effect(() => {
       this.searchTerm$.subscribe(term => {
-        this._filterRequest.set({
+        this.dataSource.filterRequest.set({
           searchTerm: term
         })
         this.load();
@@ -91,103 +68,55 @@ export class ProjectComponent {
     });
   }
 
-  private readonly _projectResource = rxResource({
-    request: () => ({
-      pageRequest: this._pageRequest(),
-      sortRequest: this._sortRequest(),
-      filterRequest: this._filterRequest()
-    }),
-    loader: ({request}) =>
-      this.dataSource.getProjects(request.pageRequest, request.sortRequest, request.filterRequest)
-  });
-
-
-  public readonly projects = computed<IProjectResponse[]>(() => {
-    return this._projectResource.value()?.items.map(project => ({
-      ...project,
-      createdOn: formatDistanceToNow(new Date(project.createdOn), {addSuffix: true}),
-      modifiedOn: formatDistanceToNow(new Date(project.modifiedOn), {addSuffix: true}),
-    })) ?? [];
-  });
-
-  public readonly total = computed(() => {
-    return this._projectResource.value()?.total ?? 0;
-  })
-
-  public readonly isLoading = computed<boolean>(() => {
-    return this._projectResource.isLoading();
-  })
-
-
-
   public load() {
-   this._sortRequest.set({
-     sortBy: 'code',
-     sortDir: 'asc',
-   })
+    this.dataSource.sortRequest.set({
+      sortBy: 'code',
+      sortDir: 'asc',
+    })
   }
 
   public createProject(): void {
     const dialogRef = this._matDialogRef.open(CreateProjectDialogComponent)
-
     dialogRef.afterClosed().subscribe((request: IProjectCreateRequest) => {
       if (!request) return;
       this.dataSource.createProject(request).subscribe({
         next: () => {
           this.load()
         },
-        error: err => {alert(err.error.detail)}
+        error: err => {
+          alert(err.error.detail)
+        }
       })
     });
   }
 
   public updateProject(projectId: string) {
     const dialogRef = this._matDialogRef.open(UpdateProjectDialogComponent)
-
     dialogRef.afterClosed().subscribe((request: IProjectUpdateRequest) => {
       if (!request) return;
       this.dataSource.updateProject(projectId, request).subscribe({
         next: () => {
           this.load()
         },
-        error: err => {alert(err.error.detail)}
+        error: err => {
+          alert(err.error.detail)
+        }
       });
     });
   }
 
   public deleteProject(projectId: string) {
     const dialogRef = this._matDialogRef.open(DeleteProjectDialogComponent)
-
     dialogRef.afterClosed().subscribe((result) => {
       if (!result) return;
       this.dataSource.deleteProject(projectId).subscribe({
         next: () => {
           this.load()
         },
-        error: err => {alert(err.error.detail)}
+        error: err => {
+          alert(err.error.detail)
+        }
       });
     });
-  }
-
-  public onPageChange($event: PageEvent) {
-    this._pageRequest.set({
-      pageNumber: $event.pageIndex + 1,
-      pageSize: $event.pageSize,
-    })
-  }
-
-  public changeSort(sortBy: string) {
-    if (this._sortRequest().sortDir === 'asc') {
-      this._sortRequest.set({
-        sortBy: sortBy,
-        sortDir: "desc"
-      });
-    }
-    else {
-      this._sortRequest.set({
-        sortBy: sortBy,
-        sortDir: "asc"
-      });
-    }
   }
 }
